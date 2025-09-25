4fb9e6553d371d4cdbd4306ec40548fa2f2ec3d0
ziadmohamed <ziad@gmail.com>
2025-09-25 12:37:32 +0300

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const { ENV_VARS } = require("./envVars");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://ziadmohamed123:0QXmG4L1hx9J3WqE@cluster0.kel3mly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://ziadmohamed123:6sFOeFPqL199DbUF@cluster0.kel3mly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("MonogDB connected: " + conn.connection.host);
   } catch (error) {
