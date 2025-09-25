1bf577355fb0cd3056096327eab096d4e0bf7cb3
ziadmohamed <ziad@gmail.com>
2025-09-25 18:45:40 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth.router");
+const MovieRoutes = require("./routes/movie.router");
+const TVRoutes = require("./routes/tv.router");
+const SearchRoutes = require("./routes/search.router");
+const { ENV_VARS } = require("./config/envVars");
+const { connectDB } = require("./config/db");
+const { protectRoute } = require("./middleware/protectRoute");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
 
-// ✅ خليها هنا قبل أي Route
+const PORT = ENV_VARS.PORT;
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", 
     credentials: true,
   })
 );
 
-const PORT = ENV_VARS.PORT;
-
-// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, MovieRoutes);
 app.use("/api/v1/tv", protectRoute, TVRoutes);
@@ -27,7 +36,11 @@ app.get("/", (req, res) => {
   });
 });
 
+// console.log("Loaded NODE_ENV:", process.env.NODE_ENV);
+
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
   connectDB();
 });
+
+// TMBD API
